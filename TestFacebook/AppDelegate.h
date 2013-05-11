e5ba25e8b245bfb7c44534a56b115023bd45e269
ivo <ivo@unknown_00-1c-b3-b3-ac-86.gateway.2wire.net>
2013-05-11 22:03:46 +0800

    Initial Commit

---
+++ TestFacebook/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestFacebook
+//
+//  Created by ivo on 11/5/13.
+//  Copyright (c) 2013 ivo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
